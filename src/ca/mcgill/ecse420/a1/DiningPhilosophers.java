9ca451201ca451c609c856d57dd5116d6bb6c22e
DavidRitch96 <david.ritch@mail.mcgill.ca>
2018-09-22 15:12:17 -0400

    Matrix must part 1&2, and beginning of Dining Philosophers problem
    Dining philosophers doesn’t work yet

--- src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -9,8 +9,14 @@ public class DiningPhilosophers {
 	public static void main(String[] args) {
 
 		int numberOfPhilosophers = 5;
-                Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
-                Object[] chopsticks = new Object[numberOfPhilosophers];
+		Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+		Object[] chopsticks = new Object[numberOfPhilosophers];
+		Thread[] threads = new Thread[numberOfPhilosophers];
+		
+		for (int i = 0; i < threads.length; i++) {
+			threads[i]=new Thread(philosophers[i]);
+			threads[i].start();
+		}
 	}
 
 	public static class Philosopher implements Runnable {
@@ -19,7 +25,7 @@ public class DiningPhilosophers {
 
 		@Override
 		public void run() {
-			
+			System.out.println("hello");
 		}
 
 
