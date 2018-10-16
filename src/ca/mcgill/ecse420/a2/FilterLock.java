736498807a0a6c01e8be46a08b54a24acd7eee3f
harvito <michael.harvey@mail.mcgill.ca>
2018-10-16 11:26:33 -0400

    added implements statement, unimplemented methods.

--- src/ca/mcgill/ecse420/a2/FilterLock.java
+++ src/ca/mcgill/ecse420/a2/FilterLock.java
@@ -1,5 +1,45 @@
 package ca.mcgill.ecse420.a2;
 
-public class FilterLock {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class FilterLock implements Lock {
+
+  @Override
+  public void lock() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void lockInterruptibly() throws InterruptedException {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public Condition newCondition() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean tryLock() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean tryLock(long arg0, TimeUnit arg1) throws InterruptedException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void unlock() {
+    // TODO Auto-generated method stub
+    
+  }
 
 }
